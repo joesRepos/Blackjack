3682a814dc38f06156923dae0781945b8e114259
joesRepos <joemitchell1717@gmail.com>
2026-02-16 16:55:29 +0000

    Create card object.

---
+++ Blackjack/Card.cs
@@ -0,0 +1,28 @@
+﻿public class Card
+{
+    public string Suit { get; set; }
+    public string Rank { get; set; }
+    
+    public int Value
+    {
+        get
+        {
+            if (Rank == "Jack" || Rank == "Queen" || Rank == "King")
+                return 10;
+            if (Rank == "Ace")
+                return 11;
+            return int.Parse(Rank);
+        }
+    }
+
+    public Card(string suit, string rank)
+    {
+        Suit = suit;
+        Rank = rank;
+    }
+
+    public override string ToString()
+    {
+        return $"{Rank} of {Suit}";
+    }
+}
