d10b53ee5b3d1465818865dcc139646aba710687
joesRepos <joemitchell1717@gmail.com>
2026-02-28 21:06:17 +0000

    Comment card class.

--- Blackjack/Card.cs
+++ Blackjack/Card.cs
@@ -1,8 +1,10 @@
 ﻿public class Card
 {
+    // Stores the suit and rank of the card, with getters and setters.
     public string Suit { get; set; }
     public string Rank { get; set; }
     
+    // The value of the card as an int.
     public int Value
     {
         get
@@ -15,12 +17,14 @@
         }
     }
 
+    // Constructor.
     public Card(string suit, string rank)
     {
         Suit = suit;
         Rank = rank;
     }
 
+    // Returns the string representation of the card.
     public override string ToString()
     {
         return $"{Rank} of {Suit}";
--- Blackjack/obj/Debug/net10.0/Blackjack.AssemblyInfo.cs
+++ Blackjack/obj/Debug/net10.0/Blackjack.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blackjack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20864924c83d4e80ef95c06c16be95bc79f3da00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+147538da02f5f40d98ba6a1d5c4249b10e9b1e4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blackjack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blackjack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
